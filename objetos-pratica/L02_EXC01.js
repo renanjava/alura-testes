10266d997a30f70c5508b92b00e4fe90c0fead5b
renanjava <renancvr777@gmail.com>
2024-09-30 16:53:07 -0300

    exc01 l2

---
+++ objetos-pratica/L02_EXC01.js
@@ -0,0 +1,44 @@
+var pessoa = {
+    nome: 'renan',
+    idade: 20,
+    solteiro: true,
+    hobbies: ['trilha de moto', 'jogar', 'ouvir musica'],
+}
+
+var pessoa2 = {
+    nome: 'renan2',
+    idade: 20,
+    solteiro: true,
+    hobbies: ['trilha de moto', 'jogar', 'ouvir musica'],
+}
+
+var pessoa3 = {
+    nome: 'renan3',
+    idade: 20,
+    solteiro: true,
+    hobbies: ['trilha de moto', 'jogar', 'ouvir musica'],
+}
+
+pessoa = {
+    ...pessoa,
+    endereco: {
+        rua: 'itambe',
+        cidade: 'itambe',
+        estado: 'itambe',
+    },
+}
+
+function mostrarInfoPessoa(pessoa) {
+    console.log(
+        typeof pessoa.nome + ' ' + pessoa.nome + '\n' +
+        typeof pessoa.idade + ' ' + pessoa.idade + '\n' +
+        typeof pessoa.solteiro + ' ' + pessoa.solteiro + '\n' +
+        typeof pessoa.hobbies + ' ' + pessoa.hobbies + '\n',
+        typeof pessoa.endereco.rua + ' ' + pessoa.endereco.rua + '\n' +
+        typeof pessoa.endereco.cidade + ' ' + pessoa.endereco.cidade + '\n' +
+        typeof pessoa.endereco.estado + ' ' + pessoa.endereco.estado + '\n',
+    );
+
+}
+
+mostrarInfoPessoa(pessoa);
