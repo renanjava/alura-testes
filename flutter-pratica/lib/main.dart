11dec3f5ccc07aae3bb0f00713c1665fcf5432d7
renanjava <renancvr777@gmail.com>
2024-10-24 14:28:46 -0300

    entendendo o card e listtile

--- flutter-pratica/lib/main.dart
+++ flutter-pratica/lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text('Transferências'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          Card(
+              child: ListTile(
+            leading: Icon(Icons.monetization_on),
+          )),
+          Card(
+              child: ListTile(
+            leading: Icon(Icons.monetization_on),
+          )),
+          Card(
+              child: ListTile(
+            leading: Icon(Icons.monetization_on),
+          )),
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
     ),
-    floatingActionButton:
-        FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-  )));
+  ));
 }
