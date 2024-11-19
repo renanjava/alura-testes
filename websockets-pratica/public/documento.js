33ef868f0997bbc51aed5d0d5fd8c4717a952daf
renanjava <renancvr777@gmail.com>
2024-11-19 15:16:11 -0300

    adicionando o conceito de salas usando join() e to()

--- websockets-pratica/public/documento.js
+++ websockets-pratica/public/documento.js
@@ -1,7 +1,12 @@
-import { emitirTextoEditor } from "./socket-front-documento.js";
+import { emitirTextoEditor, selecionarDocumento } from "./socket-front-documento.js";
 
-
+const parametros = new URLSearchParams(window.location.search)
+const nomeDocumento = parametros.get("nome")
+const nomePagina = document.getElementById("titulo-documento")
+nomePagina.textContent = nomeDocumento || "Documento sem título"
 const textoEditor = document.getElementById("editor-texto")
+
+selecionarDocumento(nomeDocumento)
 
 textoEditor.addEventListener("keyup", () => {
     emitirTextoEditor(textoEditor.value);
--- websockets-pratica/src/socket.back.js
+++ websockets-pratica/src/socket.back.js
@@ -3,7 +3,12 @@ import io from "./servidor.js";
 io.on("connection", (socket) => {
     console.log(`um cliente se conectou ${socket.id}`);
 
+    socket.on("selecionar_documento", (nome) => {
+        socket.join(nome)
+    })
+
     socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto)
+        //socket.broadcast.emit("texto_editor_clientes", texto)
+        socket.to("JavaScript").emit("texto_editor_clientes", texto)
     });
 });
