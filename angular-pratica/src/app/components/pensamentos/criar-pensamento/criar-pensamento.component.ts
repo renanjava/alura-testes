5dad8c194eb0a52b836b0708261047eb2e1431f4
Renan <renancvr777@gmail.com>
2024-12-22 14:22:00 -0300

    angular: property binding, event binding e two-way binding

--- angular-pratica/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-pratica/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit{
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo2',
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert("novo pensamento criado")
+  }
+
+  cancelarPensamento(){
+    alert("pensamento cancelado")
+  }
 }
