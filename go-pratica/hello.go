804c3a3af7789754f31e6f634de56eb837310032
Renan <renancvr777@gmail.com>
2025-01-25 00:56:27 -0300

    go: conhecendo funcoes e condicionais

--- go-pratica/hello.go
+++ go-pratica/hello.go
@@ -2,25 +2,58 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	nome := "Renan"
-	idade := 20
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func devolveNomeEIdade() (string, int) {
+	nome := "Douglas"
+	idade := 24
+	return nome, idade
+}
+
+func exibeIntroducao() {
+	nome, idade := devolveNomeEIdade()
 	versao := 1.1
+	fmt.Println("Olá, sr.", nome, "de", idade, "anos")
+	fmt.Println("Este programa está na versão", versao)
+}
 
-	var nome2 = "Renan"
-	var idade2 = 20
-	var versao2 = 1.1
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
 
-	var nome3 string = "Renan"
-	var idade3 int = 20
-	var versao3 float64 = 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
-	fmt.Println("Programa na versão", versao)
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
 
-	fmt.Println("Tipos de variáveis:", reflect.TypeOf(nome), reflect.TypeOf(idade), reflect.TypeOf(versao))
-	fmt.Println("Tipos de variáveis:", reflect.TypeOf(nome2), reflect.TypeOf(idade2), reflect.TypeOf(versao2))
-	fmt.Println("Tipos de variáveis:", reflect.TypeOf(nome3), reflect.TypeOf(idade3), reflect.TypeOf(versao3))
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	//site := "https://www.alura.com.br"
+	//resp, err := http.Get(site)
 }
