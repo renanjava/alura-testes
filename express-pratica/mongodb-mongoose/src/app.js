a3e73a828cb678dbc8a62bb828ffc122a0f7219c
renanplin <renan.leao@plinenergia.com.br>
2025-05-29 09:19:50 -0300

    feat: add rotas livro e remove heroes, favorites e players

--- express-pratica/mongodb-mongoose/src/app.js
+++ express-pratica/mongodb-mongoose/src/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import { databaseConnection } from "./config/dbConnect.js"
 
 const connection = await databaseConnection()
@@ -8,131 +8,51 @@ connection.once("open", () => console.log("Conexão com o banco feita com sucess
 const app = express()
 app.use(express.json())
 
-const heroes = [
-    {
-        id: "1",
-        heroName: "Zarya",
-        players: []
-    },
-    {
-        id: "2",
-        heroName: "Soldier 76",
-        players: []
-    },
+const livros = [
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis"
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit"
+  }
 ]
 
-const players = [
-    {
-        id: "1",
-        playerName: "Dafran",
-        heroes: []
-    },
-    {
-        id: "2",
-        playerName: "Harbleu",
-        heroes: []
-    },
-]
-
-const favorites = [
-    {
-        idPlayer: "1",
-        idHero: "1",
-    },
-    {
-        idPlayer: "1",
-        idHero: "2",
-    },
-    {
-        idPlayer: "2",
-        idHero: "1",
-    },
-    {
-        idPlayer: "2",
-        idHero: "2",
-    },
-]
-
-function buscaPlayers(idPlayer){
-    return players.findIndex((player) => player.id === idPlayer)
-}
-
-function buscaHeroes(idHero){
-    return heroes.findIndex((hero) => hero.id === idHero)
-}
-
-function buscaFavorites(idFavorite){
-    return favorites.findIndex((favorite) => favorite.id === idFavorite)
+function buscaLivro(id) {
+  return livros.findIndex(livro => {
+    return livro.id === Number(id);
+  })
 }
 
 app.get("/", (req, res) => {
-    res.status(200).send("Overwatch 2 - Hero Picker")
+  res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
 })
 
-app.get("/player", (req, res) => {
-    res.status(200).json(players)
-})
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("livro cadastrado com sucesso");
+});
 
-app.post("/player", (req, res) => {
-    players.push(req.body)
-    res.status(201).json(players)
-})
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
 
-app.put("/player/:id", (req, res) => {
-    const index = buscaPlayers(req.params.id)
-    players[index].playerName = req.body.playerName
-    res.status(200).json(players[index])
-})
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
 
-app.delete("/player/:id", (req, res) => {
-    const index = buscaPlayers(req.params.id)
-    players.splice(index, 1)
-    res.status(200).json(players)
-})
-
-app.get("/player/:id", (req, res) => {
-    const index = buscaPlayers(req.params.id)
-    res.status(200).json(players[index])
-})
-
-app.get("/favorite", (req, res) => {
-    res.status(200).json(favorites)
-})
-
-app.post("/favorite", (req, res) => {
-    favorites.push(req.body)
-    res.status(201).json(favorites)
-})
-
-app.delete("/favorite/:id", (req, res) => {
-    const index = buscaFavorites(req.params.id)
-    favorites.splice(index, 1)
-    res.status(200).json(favorites)
-})
-
-app.get("/player/:id/favorite", (req, res) => {
-    const index = buscaPlayers(req.params.id)
-    res.status(200).json(players[index].heroes)
-})
-
-app.get("/hero", (req, res) => {
-    res.status(200).json(heroes)
-})
-
-app.get("/hero/:id", (req, res) => {
-    const index = buscaHeroes(req.params.id)
-    res.status(200).json(heroes[index])
-})
-
-app.delete("/hero/:id", (req, res) => {
-    const index = buscaHeroes(req.params.id)
-    heroes.splice(index, 1)
-    res.status(200).json(heroes)
-})
-
-app.post("/hero", (req, res) => {
-    heroes.push(req.body)
-    res.status(201).json(heroes)
-})
-
-export default app
+export default app;
