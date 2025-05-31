4239dc59fa9b211a55d396d6b872031fbcff012d
Renan <renancvr777@gmail.com>
2025-05-31 17:43:35 -0300

    express: small things

--- express-pratica/mongodb-mongoose/server.js
+++ express-pratica/mongodb-mongoose/server.js
@@ -1,8 +1,6 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log("servidor escutando!");
 });
